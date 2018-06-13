e7456b323930f969c8e31cc1c76e9bc9d3c71a7a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-13 17:50:13 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,17 @@
 def my_each(array)
   
 i=0 
-
+newarray=[]
 while i<array.length  
 newarray << array[i]
-
+yield(array)
 i+=1 
 end 
 
 newarray 
   
 end   
+
+my_each(array) do |n|
+newarray
+end 
