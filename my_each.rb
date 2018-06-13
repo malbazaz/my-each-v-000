a478f09fda86c273f450f51279cea4db12cadf01
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-13 17:51:16 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
-end
+
+def my_each(array)
+  
+i=0 
+newarray=[]
+while i<array.length  
+newarray << array[i]
+yield(array[i])
+i+=1 
+end 
+
+newarray 
+  
+end   
